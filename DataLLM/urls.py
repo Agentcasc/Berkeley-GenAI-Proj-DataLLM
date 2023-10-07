b3a4dc03b7fd8a983ce015ba091914e2273fa591
Yanjie Zheng <yanjiezheng@berkeley.edu>
2023-10-07 11:22:34 -0700

    changed dir

--- DataLLM/urls.py
+++ DataLLM/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include # new
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import path
+from django.http import HttpResponse
+from django.conf.urls import include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls")), # new
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]

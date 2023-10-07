ceb29cf1fb2f54dbe55b801daa491f6493869caf
Yanjie Zheng <yanjiezheng@berkeley.edu>
2023-10-07 11:36:25 -0700

    a

--- DataLLM/urls.py
+++ DataLLM/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls")), # new
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
